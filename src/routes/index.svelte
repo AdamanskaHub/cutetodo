9f81960b986f504c850932944cc10c9993a87f9b
Emilie <adamanska@gmail.com>
2022-01-03 11:20:43 +0100

    updating todo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import supabase from '$lib/db';
+	import { onMount } from 'svelte';
+	import Todo from '$lib/Todo.svelte';
+
+	let todos = ['hfeiwh'];
+	onMount(async () => {
+		let { data, error } = await supabase.from('todos').select('*');
+		todos = data;
+	});
+	const updateTodo = (todo) => {
+		// try {
+		// 	const { data, error } = await supabase
+		// 		.from('todos')
+		// 		.update({ task: todo.task })
+		// 		.eq('id', 'todo.id');
+		// } catch (err) {
+		// 	console.log(err);
+		// }
+		console.table(todo);
+	};
+</script>
+
+{#each todos as todo}
+	<Todo {todo} {updateTodo} />
+{:else}
+	<p>no tasks</p>
+{/each}
